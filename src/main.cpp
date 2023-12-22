ac50682ae0c992e13b728163b6a99fc3ca819b42
CritBit <Jakub.Sadowski1@gmail.com>
2023-12-22 18:44:15 +0100

    skull emoji

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+
+
+
+#include <Arduino.h>
+#include "BLESerial.h"
+
+BLESerial bleSerial;
+
+void setup() {
+    Serial.begin(115200);
+
+    // Create the BLE Device
+    bleSerial.begin("Cyberbadge2.0");
+
+}
+
+void loop() {
+    bleSerial.loop();
+    
+}
